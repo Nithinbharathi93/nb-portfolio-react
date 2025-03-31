34cfa9876fe3377edd1c7e038f690f0951014f3f
Nithinbharathi93 <nithinthelordest@gmail.com>
2025-04-01 00:33:24 +0530

    successfully added tailwind

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,36 +1,36 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg";
+
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const onButtonClick = () => {
     fetch("nb_resume_final.pdf").then((response) => {
-        response.blob().then((blob) => {
-            const fileURL =
-                window.URL.createObjectURL(blob);
-            let alink = document.createElement("a");
-            alink.href = fileURL;
-            alink.download = "nb_resume_final.pdf";
-            alink.click();
-        });
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "nb_resume_final.pdf";
+        alink.click();
+      });
     });
-};
+  };
 
   return (
-    <nav className="nav-main">
-        <img className="nav-logo" src={logo} alt="Logo" />
-        <h1 className="nav-head">nithinthelordest@gmail.com</h1>
-        <ul className="nav-items">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/portfolio">Portfolio</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/experience">Experiencs</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link onClick={onButtonClick}>Resume</Link></li>
-        </ul>
+    <nav className="flex justify-between items-center w-full bg-[#0f0715] text-white p-4">
+      <img className="w-12 nav-logo" src={logo} alt="Logo" />
+      <h1 className="hidden md:block pl-2">nithinthelordest@gmail.com</h1>
+      <ul className="flex flex-wrap justify-evenly w-1/2 mr-10 list-none items-center md:hidden">
+        <li className="pr-3"><Link to="/">Home</Link></li>
+        <li className="pr-3"><Link to="/about">About</Link></li>
+        <li className="pr-3"><Link to="/portfolio">Portfolio</Link></li>
+        <li className="pr-3"><Link to="/services">Services</Link></li>
+        <li className="pr-3"><Link to="/experience">Experiences</Link></li>
+        <li className="pr-3"><Link to="/contact">Contact</Link></li>
+        <li className="pr-3"><Link onClick={onButtonClick}>Resume</Link></li>
+      </ul>
     </nav>
   );
 };
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
     return (
         <div className="flex justify-center items-center h-screen bg-blue-500">
-          <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
+          <h1 className="text-4xl text-white font-bold">Hello everyone</h1>
         </div>
       );
 }
